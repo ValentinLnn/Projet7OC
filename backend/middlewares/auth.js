969b5745201fd1fe3042687bc7d4bdba388a28f5
Valentin Launay <otctrojan@gmail.com>
2024-02-28 16:04:00 +0100

    bearer autorization check

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    try {
+    try {  
+        const authorizationHeader = req.headers.authorization;
+
+        if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
         const userId = decodedToken.userId;
